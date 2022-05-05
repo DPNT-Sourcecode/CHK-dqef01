619380bfae357c8736e0fcbb9c92472eaa27f1ae
sbx_user1051 <sbx_user1051@169.254.158.253>
2022-05-05 16:14:12 +0000

    Thu May 05 16:14:12 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,17 +3,13 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
+# @app.route("/")
 def hello(friend_name):
     return("Hello World!" + friend_name)
 
-if __name__ == "__main__":
-    app.run()
-
-
-
-
+# if __name__ == "__main__":
+#     app.run()
