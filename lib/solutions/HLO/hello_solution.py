ea912cbbbcbab734df40f6cebe5b3541f2da4682
sbx_user1051 <sbx_user1051@169.254.158.253>
2022-05-05 16:08:12 +0000

    Thu May 05 16:08:12 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -8,10 +8,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello(friend_name):
+def hello():
     return("Hello World!")
 
 if __name__ == "__main__":
     app.run()
+
 
 
