c4ca3331cd50c3418e34eec5fd83ee644e661848
sbx_user1051 <sbx_user1051@169.254.158.253>
2022-05-05 23:32:37 +0000

    Thu May 05 23:32:37 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -70,20 +70,24 @@ def calc_product(sku, qtt_product):
 
         for item in list_offers:
             offer = Offer(**item)
-            qtt_offer = int(qtt_product / offer.qtt)
-            amount = qtt_offer * offer.price
-            rest = (qtt_product - (qtt_offer * offer.qtt))
-            more_offers = list(filter(lambda x: x['qtt'] <= rest, list_offers))
-            print(amount)
-            
-            if more_offers:
-                new_offer = Offer(**more_offers[-1])
-                qtt_new_offer = int(rest / new_offer.qtt)
-                amount_new = qtt_new_offer * new_offer.price
-                new_rest = (rest - (qtt_new_offer * new_offer.price))
-                amount = amount + amount_new + (new_rest * product.price)
 
-            amount = amount + (rest * product.price)
+            if (offer.qtt <= rest):
+
+                qtt_offer = int(qtt_product / offer.qtt)
+                amount = qtt_offer * offer.price
+                rest = (qtt_product - (qtt_offer * offer.qtt))
+
+            # more_offers = list(filter(lambda x: x['qtt'] <= rest, list_offers))
+            # print(amount)
+            
+            # if more_offers:
+            #     new_offer = Offer(**more_offers[-1])
+            #     qtt_new_offer = int(rest / new_offer.qtt)
+            #     amount_new = qtt_new_offer * new_offer.price
+            #     new_rest = (rest - (qtt_new_offer * new_offer.price))
+            #     amount = amount + amount_new + (new_rest * product.price)
+
+                amount = amount + (rest * product.price)
     else:
         amount = product.price * qtt_product
 
@@ -106,7 +110,3 @@ def checkout(skus):
             return -1
                 
 print(checkout('AAAAA'))                
-
-
-
-
