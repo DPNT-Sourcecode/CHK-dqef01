6798ebdea01003aa91dd08a39fab1502a2fcd61c
sbx_user1051 <sbx_user1051@169.254.158.253>
2022-05-05 22:44:39 +0000

    Thu May 05 22:44:39 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -63,10 +63,19 @@ def calc_product(sku, qtt_product):
 
     if list_offers:
         for item in list_offers.sort(key=lambda x:x["qtt"], reverse=True):
-            offer = Offer(**list_offers[-1])
+            offer = Offer(**item)
             qtt_offer = int(qtt_product / offer.qtt)
             amount = qtt_offer * offer.price
             rest = (qtt_product - (qtt_offer * offer.qtt))
+            more_offers = list(filter(lambda x: x['qtt'] >= rest, list_offers))
+
+            if more_offers:
+                new_offer = Offer(**more_offers[-1])
+                qtt_new_offer = int(rest / new_offer.qtt)
+                amount_new = qtt_new_offer * new_offer.price
+                new_rest = (rest - (qtt_new_offer * new_offer.price))
+                amount = amount + amount_new + (new_rest * product.price)
+
             amount = amount + (rest * product.price)
     else:
         amount = product.price * qtt_product
