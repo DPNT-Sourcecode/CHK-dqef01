b6aaee72b493dc965524d197b1dfb218cec25411
sbx_user1051 <sbx_user1051@169.254.158.253>
2022-05-05 22:06:28 +0000

    Thu May 05 22:06:28 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -71,18 +71,18 @@ def calc_product(sku, qtt_product):
 
 def checkout(skus):
     amount = 0
-    for product in products:
-        p = Product(**product)
-        qtt = skus.count(p.description)
-        product_amount = calc_product(p.description,qtt)
-        amount += product_amount
-    return amount    
 
-print(checkout('AAAaaaaaaaaaaaaaBbCD'))
+    if sum(map(str.islower, skus)) > 0:
+        return -1
+    else:
+        for product in products:
+            p = Product(**product)
+            qtt = skus.count(p.description)
+            product_amount = calc_product(p.description,qtt)
+            amount += product_amount
+        if amount == 0:
+            return -1
+        else:
+            return amount    
 
-
-
-
-
-
-
+print(checkout(''))
