8ef643ae0fe92f489c5ae3463380d077406a6bfb
sbx_user1051 <sbx_user1051@169.254.158.253>
2022-05-05 17:03:19 +0000

    Thu May 05 17:03:19 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,21 +8,24 @@ product_B = {"B": 30}
 product_C = {"C": 20}
 product_D = {"D": 15}
 
-list = []
+# list = []
 
-list.append(product_A)
-list.append(product_B)
-print(list)
-
-def checkout(skus):
-    for item in skus:
-        print(item)
-    # raise NotImplementedError()
+# list.append(product_A)
+# list.append(product_B)
+# print(list)
 
 def Product():
     def __init__(self, description, price):
         self.description = description
         self.price = price
+
+item1 = Product(product_A)     
+print(item1)
+
+def checkout(skus):
+    for item in skus:
+        print(item)
+    # raise NotImplementedError()
 
 print(checkout('AAABCD'))
 
