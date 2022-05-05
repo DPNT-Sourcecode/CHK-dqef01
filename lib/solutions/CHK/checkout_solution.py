3d38ad9cb06c14bcb630916411f523ca8983b6b8
sbx_user1051 <sbx_user1051@169.254.158.253>
2022-05-05 21:54:32 +0000

    Thu May 05 21:54:32 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
+from curses.ascii import isupper
+from itertools import count
+
+
 class Product:
     def __init__(self, description, price):
         self.description = description
@@ -70,10 +74,11 @@ def checkout(skus):
     amount = 0
     for product in products:
         p = Product(**product)
-        qtt = skus.count(p.description)
+        qtt = .isupper.count(p.description)
         product_amount = calc_product(p.description,qtt)
         amount += product_amount
     return amount    
+
 
 
 
