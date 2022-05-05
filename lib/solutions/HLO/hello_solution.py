be97c2d1aeaf53e9a0540cffd7f87f0e92c20ce2
sbx_user1051 <sbx_user1051@169.254.158.253>
2022-05-05 16:26:53 +0000

    Thu May 05 16:26:53 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -8,16 +8,18 @@
 # app = Flask(__name__)
 
 # @app.route("/")
-def hello():
+def hello(friend_name):
     try:
-        return('Hello World!')
+        if friend_name:
+            return('Hello World!')
     except:
         raise Exception('Error')    
     # return('Hello World, %s!' % (friend_name))
 
-print(hello())
+print(hello('X'))
 # if __name__ == "__main__":
 #     app.run()
+
 
 
 
