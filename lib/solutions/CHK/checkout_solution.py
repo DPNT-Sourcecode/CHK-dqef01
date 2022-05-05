9b0b5b4f51051ee61fa268857e0558e041cec049
sbx_user1051 <sbx_user1051@169.254.158.253>
2022-05-05 20:25:12 +0000

    Thu May 05 20:25:12 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,39 +2,15 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-
-product_A = {"A": 50}
-product_B = {"B": 30}
-product_C = {"C": 20}
-product_D = {"D": 15}
-
-offer_1 = {"Product": "A",
-           "Qtt": 3,
-           "price": 130}
-
-offer_2 = {"Product": "B",
-           "Qtt": 2,
-           "price": 45}
-
-# list = []
-
-# list.append(product_A)
-# list.append(product_B)
-# print(list)
-
 def Product():
     def __init__(self, description, price):
         self.description = description
         self.price = price
-
-def Offer():
-
-item1 = Product(product_A)     
-print(item1.description)
-
+        
 def checkout(skus):
     for item in skus:
         print(item)
     # raise NotImplementedError()
 
 print(checkout('AAABCD'))
+
