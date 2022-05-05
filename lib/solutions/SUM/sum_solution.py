2c2a5a02bced23bef86920502ff064aceda93c54
sbx_user1051 <sbx_user1051@169.254.158.253>
2022-05-05 15:51:01 +0000

    Thu May 05 15:51:01 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,12 +1,12 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if x <= 0 or y <= 0:
-        print('Variable should be greater than 0')    
-
+    
     if type(x) is int and type(y) is int:
+        if x <= 0 or y <= 0:
+            print('Variable should be greater than 0')    
+
         return x + y
     else:
         print('Variable should be integer')    
      
-# compute(1,2)
-compute('A',2)
+print(compute('A',2))
