71a32bb0f3fe8fa03ec9a30f0cac478f9a64efc8
sbx_user1051 <sbx_user1051@169.254.2.197>
2022-05-06 09:11:29 +0000

    Fri May 06 09:11:29 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -105,28 +105,23 @@ def checkout(skus):
             qtt = skus.count(p.description)
             product_amount = calc_product(p.description,qtt)
             amount += product_amount
-
+            
             list_offers = list(filter(lambda x: x['description'] == p.description, offers))
             
-            print(list_offers)
             if list_offers:
                 offer = Offer(**list_offers[-1])
 
                 if offer.get_one_free != "":
                     list_products_free = list(filter(lambda x: x['description'] == offer.get_one_free, products))
-                    print(list_products_free) 
                     product_for_free = Product(**list_products_free[-1])
                     qtt_for_free = skus.count(offer.get_one_free)
 
                     if qtt_for_free > 0:
-                        print('passed here')
-                        print(amount)
                         amount = amount - product_for_free.price
-                        print(amount)
-
+                        
         if amount > 0 or skus == "":
             return amount
         else:
             return -1
                 
-print(checkout('EEEEB'))                
+print(checkout('AABBCC'))                
